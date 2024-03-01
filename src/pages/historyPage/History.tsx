96f63c12b5162a39febe8bdbdfa224e773c0195a
Luka Chubinidze <chubinidzel08@gmail.com>
2024-03-01 19:04:58 +0400

    started using context

--- src/pages/historyPage/History.tsx
+++ src/pages/historyPage/History.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from "./History.module.css";
 import ImagesGridCont from "../../components/imagesGridCont/ImagesGridCont";
 import WordsGrid from "../../components/wordsGrid/WordsGrid";
 import HistoryMessage from "../../components/historyMessage/HistoryMessage";
+import { WordsHistoryContext } from "../../context/wordsHistoryProvider/WordsHistoryProvider";
 
 function History() {
+  const { wordsArr, setWordsArr } = useContext(WordsHistoryContext);
   const value = false;
+
+  console.log(wordsArr);
 
   return (
     <main className={styles.main}>
@@ -16,7 +20,7 @@ function History() {
         </div>
         <div className={styles.content}>
           {/* If any word is selected, show the result; if not, show the message */}
-          {value ? <ImagesGridCont data={[]} /> : <HistoryMessage />}
+          {value ? <ImagesGridCont data={[]} loader /> : <HistoryMessage />}
         </div>
       </section>
     </main>
